1ce764f037607f1349d01f8eaf2a64c43e6d6c34
Justin Huang <jstn@cs.washington.edu>
2016-02-17 20:06:42 -0800

    Increase server wait time more for Travis.

--- test/blinky_test.cc
+++ test/blinky_test.cc
@@ -66,7 +66,7 @@ class MockBlinkyServer {
 
 class BlinkyTest : public ::testing::Test {
  public:
-  BlinkyTest() : node_handle_(), server_(), blinky_(5) {}
+  BlinkyTest() : node_handle_(), server_(), blinky_(10) {}
 
   void SetUp() {}
 
