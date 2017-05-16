f4958f1a8cc80fa477a8c8922b225b3e7d9ba925
Justin Huang <jstn@cs.washington.edu>
2017-05-15 18:05:53 -0700

    Updated names to work with namespaces.

--- rapid_pbd/include/rapid_pbd/action_names.h
+++ rapid_pbd/include/rapid_pbd/action_names.h
@@ -6,21 +6,21 @@
 
 namespace rapid {
 namespace pbd {
-static const char kProgramActionName[] = "rapid_pbd/execute_program_action";
+static const char kProgramActionName[] = "execute_program_action";
 
-static const char kGripperActionName[] = "rapid_pbd/gripper_action";
-static const char kLeftGripperActionName[] = "rapid_pbd/l_gripper_action";
-static const char kRightGripperActionName[] = "rapid_pbd/r_gripper_action";
+static const char kGripperActionName[] = "gripper_action";
+static const char kLeftGripperActionName[] = "l_gripper_action";
+static const char kRightGripperActionName[] = "r_gripper_action";
 
-static const char kArmJointActionName[] = "rapid_pbd/arm_joint_action";
-static const char kLeftArmJointActionName[] = "rapid_pbd/l_arm_joint_action";
-static const char kRightArmJointActionName[] = "rapid_pbd/r_arm_joint_action";
+static const char kArmJointActionName[] = "arm_joint_action";
+static const char kLeftArmJointActionName[] = "l_arm_joint_action";
+static const char kRightArmJointActionName[] = "r_arm_joint_action";
 
 namespace pr2 {
 static const char kLeftGripperActionName[] =
-    "l_gripper_controller/gripper_action";
+    "/l_gripper_controller/gripper_action";
 static const char kRightGripperActionName[] =
-    "r_gripper_controller/gripper_action";
+    "/r_gripper_controller/gripper_action";
 }  // namespace pr2
 }  // namespace pbd
 }  // namespace rapid
