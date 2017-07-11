141ba8cf2dac960324f55887e7b049edd49114e1
Justin Huang <jstn@cs.washington.edu>
2017-07-10 20:08:23 -0700

    Use robot config in program execution node.

--- rapid_pbd/src/program_execution_node.cpp
+++ rapid_pbd/src/program_execution_node.cpp
@@ -7,6 +7,7 @@
 #include "rapid_pbd/action_executor.h"
 #include "rapid_pbd/action_names.h"
 #include "rapid_pbd/program_executor.h"
+#include "rapid_pbd/robot_config.h"
 #include "ros/ros.h"
 #include "std_msgs/Bool.h"
 
@@ -18,18 +19,30 @@ int main(int argc, char** argv) {
   ros::Publisher is_running_pub =
       nh.advertise<std_msgs::Bool>("is_running", 5, true);
 
-  pbd::ActionClients action_clients;
-  int num_arms;
-  bool success = ros::param::get("num_arms", num_arms);
-  if (!success) {
-    ROS_ERROR("num_arms param must be set.");
+  std::string robot("");
+  bool is_robot_specified = ros::param::get("robot", robot);
+  if (!is_robot_specified) {
+    ROS_ERROR("robot param must be specified.");
     return 1;
   }
+
+  pbd::RobotConfig* robot_config;
+  if (robot == "pr2") {
+    robot_config = new pbd::Pr2RobotConfig();
+  } else if (robot == "fetch") {
+    ROS_ERROR("Unsupported robot \"%s\"", robot.c_str());
+    return 1;
+  } else {
+    ROS_ERROR("Unsupported robot \"%s\"", robot.c_str());
+    return 1;
+  }
+
+  pbd::ActionClients action_clients;
   while (!action_clients.head_client.waitForServer(ros::Duration(5)) &&
          ros::ok()) {
     ROS_WARN("Waiting for head server.");
   }
-  if (num_arms == 1) {
+  if (robot_config->num_arms() == 1) {
     while (!action_clients.gripper_client.waitForServer(ros::Duration(5)) &&
            ros::ok()) {
       ROS_WARN("Waiting for gripper server.");
@@ -38,7 +51,7 @@ int main(int argc, char** argv) {
            ros::ok()) {
       ROS_WARN("Waiting for arm joint server.");
     }
-  } else if (num_arms == 2) {
+  } else if (robot_config->num_arms() == 2) {
     while (!action_clients.l_gripper_client.waitForServer(ros::Duration(5)) &&
            ros::ok()) {
       ROS_WARN("Waiting for left gripper server.");
@@ -74,5 +87,8 @@ int main(int argc, char** argv) {
   server.Start();
   ROS_INFO("RapidPbD program executor ready.");
   ros::spin();
+  if (robot_config) {
+    delete robot_config;
+  }
   return 0;
 }
