02bcc171d173f33f0ee6b3164ada37cc54bc2658
Justin Huang <jstn@cs.washington.edu>
2017-07-10 21:27:26 -0700

    Outline implementation of MotionPlanning.

--- rapid_pbd/include/rapid_pbd/motion_planning.h
+++ rapid_pbd/include/rapid_pbd/motion_planning.h
@@ -4,25 +4,28 @@
 #include <string>
 
 #include "geometry_msgs/Pose.h"
+#include "moveit_goal_builder/builder.h"
+#include "moveit_msgs/MoveGroupAction.h"
 #include "rapid_pbd_msgs/Landmark.h"
+
+#include "rapid_pbd/robot_config.h"
 
 namespace rapid {
 namespace pbd {
 class MotionPlanning {
  public:
-  MotionPlanning();
+  MotionPlanning(const RobotConfig& robot_config);
   void AddPoseGoal(const std::string& actuator_group,
                    const geometry_msgs::Pose& pose,
                    const rapid_pbd_msgs::Landmark& landmark);
   void AddJointGoal(const std::string& actuator_group,
                     const geometry_msgs::Pose& pose);
   void ClearGoals();
-
-  void Start();
-  void IsDone();
-  void Cancel();
+  void BuildGoal(moveit_msgs::MoveGroupGoal* goal);
 
  private:
+  const RobotConfig& robot_config_;
+  moveit_goal_builder::Builder builder_;
 };
 }  // namespace pbd
 }  // namespace rapid
--- rapid_pbd/src/motion_planning.cpp
+++ rapid_pbd/src/motion_planning.cpp
@@ -1,21 +1,47 @@
 #include "rapid_pbd/motion_planning.h"
 
+#include <map>
+#include <string>
+
+#include "geometry_msgs/Pose.h"
+#include "moveit_goal_builder/builder.h"
+#include "moveit_msgs/MoveGroupAction.h"
+#include "rapid_pbd_msgs/Landmark.h"
+
+#include "ros/ros.h"
+
+using std::string;
+
 namespace rapid {
 namespace pbd {
-MotionPlanning::MotionPlanning() {}
-void MotionPlanning::AddPoseGoal(const std::string& actuator_group,
+MotionPlanning::MotionPlanning(const RobotConfig& robot_config)
+    : robot_config_(robot_config),
+      builder_(robot_config.planning_frame(), robot_config.planning_group()) {}
+
+void MotionPlanning::AddPoseGoal(const string& actuator_group,
                                  const geometry_msgs::Pose& pose,
-                                 const rapid_pbd_msgs::Landmark& landmark) {}
+                                 const rapid_pbd_msgs::Landmark& landmark) {
+  string ee_link = robot_config_.ee_frame_for_group(actuator_group);
+  if (ee_link == "") {
+    ROS_ERROR("Unable to look up EE link for actuator group \"%s\"",
+              actuator_group.c_str());
+    return;
+  }
 
-void MotionPlanning::AddJointGoal(const std::string& actuator_group,
-                                  const geometry_msgs::Pose& pose) {}
+  // TODO: Transform pose into planning frame.
 
-void MotionPlanning::ClearGoals() {}
+  builder_.AddPoseGoal(ee_link, pose);
+}
 
-void MotionPlanning::Start() {}
+void MotionPlanning::AddJointGoal(const string& actuator_group,
+                                  const geometry_msgs::Pose& pose) {
+  // TODO: use moveit instead of joint controllers
+}
 
-void MotionPlanning::IsDone() {}
-
-void MotionPlanning::Cancel() {}
+void MotionPlanning::ClearGoals() {
+  std::map<string, geometry_msgs::Pose> goals;
+  // Overrides joint goals if any and deletes pose goals.
+  builder_.SetPoseGoals(goals);
+}
 }  // namespace pbd
 }  // namespace rapid
