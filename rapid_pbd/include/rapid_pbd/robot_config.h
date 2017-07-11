7ba7fcefc23f566009d4c19a9af6afb2c59bd3eb
Justin Huang <jstn@cs.washington.edu>
2017-07-10 20:43:48 -0700

    Make RobotConfig methods const and fix PR2 planning group.

--- rapid_pbd/include/rapid_pbd/robot_config.h
+++ rapid_pbd/include/rapid_pbd/robot_config.h
@@ -9,28 +9,30 @@ namespace pbd {
 class RobotConfig {
  public:
   virtual ~RobotConfig() {}
-  virtual std::string planning_frame() = 0;
-  virtual std::string planning_group() = 0;
-  virtual std::string base_link() = 0;
-  virtual std::string torso_link() = 0;
-  virtual std::string ee_frame_for_group(const std::string& actuator_group) = 0;
-  virtual void joints_for_group(const std::string& actuator_group,
-                                std::vector<std::string>* joint_names) = 0;
-  virtual int num_arms() = 0;
-  virtual double gripper_open_position() = 0;
+  virtual std::string planning_frame() const = 0;
+  virtual std::string planning_group() const = 0;
+  virtual std::string base_link() const = 0;
+  virtual std::string torso_link() const = 0;
+  virtual std::string ee_frame_for_group(
+      const std::string& actuator_group) const = 0;
+  virtual void joints_for_group(
+      const std::string& actuator_group,
+      std::vector<std::string>* joint_names) const = 0;
+  virtual int num_arms() const = 0;
+  virtual double gripper_open_position() const = 0;
 };
 
 class Pr2RobotConfig : public RobotConfig {
  public:
-  std::string planning_frame();
-  std::string planning_group();
-  std::string base_link();
-  std::string torso_link();
-  std::string ee_frame_for_group(const std::string& actuator_group);
+  std::string planning_frame() const;
+  std::string planning_group() const;
+  std::string base_link() const;
+  std::string torso_link() const;
+  std::string ee_frame_for_group(const std::string& actuator_group) const;
   void joints_for_group(const std::string& actuator_group,
-                        std::vector<std::string>* joint_names);
-  int num_arms();
-  double gripper_open_position();
+                        std::vector<std::string>* joint_names) const;
+  int num_arms() const;
+  double gripper_open_position() const;
 };
 }  // namespace pbd
 }  // namespace rapid
--- rapid_pbd/src/robot_config.cpp
+++ rapid_pbd/src/robot_config.cpp
@@ -6,12 +6,12 @@ using rapid_pbd_msgs::Action;
 
 namespace rapid {
 namespace pbd {
-std::string Pr2RobotConfig::planning_frame() { return "base_link"; }
-std::string Pr2RobotConfig::planning_group() { return "base_link"; }
-std::string Pr2RobotConfig::base_link() { return "base_link"; }
-std::string Pr2RobotConfig::torso_link() { return "torso_lift_link"; }
+std::string Pr2RobotConfig::planning_frame() const { return "base_link"; }
+std::string Pr2RobotConfig::planning_group() const { return "arms"; }
+std::string Pr2RobotConfig::base_link() const { return "base_link"; }
+std::string Pr2RobotConfig::torso_link() const { return "torso_lift_link"; }
 std::string Pr2RobotConfig::ee_frame_for_group(
-    const std::string& actuator_group) {
+    const std::string& actuator_group) const {
   if (actuator_group == Action::LEFT_ARM) {
     return "l_wrist_roll_link";
   } else if (actuator_group == Action::RIGHT_ARM) {
@@ -20,8 +20,9 @@ std::string Pr2RobotConfig::ee_frame_for_group(
     return "";
   }
 }
-void Pr2RobotConfig::joints_for_group(const std::string& actuator_group,
-                                      std::vector<std::string>* joint_names) {
+void Pr2RobotConfig::joints_for_group(
+    const std::string& actuator_group,
+    std::vector<std::string>* joint_names) const {
   if (actuator_group == Action::LEFT_ARM) {
     joint_names->clear();
     joint_names->push_back("l_shoulder_pan_joint");
@@ -42,8 +43,8 @@ void Pr2RobotConfig::joints_for_group(const std::string& actuator_group,
     joint_names->push_back("r_wrist_roll_joint");
   }
 }
-int Pr2RobotConfig::num_arms() { return 2; }
-double Pr2RobotConfig::gripper_open_position() { return 0.08; }
+int Pr2RobotConfig::num_arms() const { return 2; }
+double Pr2RobotConfig::gripper_open_position() const { return 0.08; }
 }  // namespace pbd
 }  // namespace rapid
 
