051962a4c25078d4f534cda2783490be68c24145
Justin Huang <jstn@cs.washington.edu>
2017-07-12 23:01:12 -0700

    Visualize end-effector poses using IK solver.

--- rapid_pbd/src/world.cpp
+++ rapid_pbd/src/world.cpp
@@ -7,9 +7,12 @@
 #include "rapid_pbd_msgs/Landmark.h"
 #include "rapid_pbd_msgs/Program.h"
 #include "rapid_pbd_msgs/Step.h"
+#include "transform_graph/graph.h"
 
 #include "rapid_pbd/joint_state.h"
 #include "rapid_pbd/robot_config.h"
+
+#include "ros/ros.h"
 
 namespace msgs = rapid_pbd_msgs;
 
@@ -22,11 +25,11 @@ void GetWorld(const RobotConfig& robot_config, const msgs::Program& program,
   world->joint_state = js;
   world->surface_box_landmarks.clear();
 
-  for (size_t i = 0; i <= step_id; ++i) {
+  for (size_t step_i = 0; step_i <= step_id; ++step_i) {
     if (program.steps.size() == 0) {
       break;
     }
-    const msgs::Step& step = program.steps[i];
+    const msgs::Step& step = program.steps[step_i];
     if (step.scene_id != "") {
       world->scene_id = step.scene_id;
     }
@@ -58,7 +61,33 @@ void GetWorld(const RobotConfig& robot_config, const msgs::Program& program,
           world->joint_state.SetPosition(name, position);
         }
       } else if (action.type == msgs::Action::MOVE_TO_CARTESIAN_GOAL) {
-        // TODO: fill this in.
+        std::vector<std::string> joint_names;
+        std::vector<double> joint_values;
+
+        transform_graph::Graph graph;
+        graph.Add("landmark",
+                  transform_graph::RefFrame(robot_config.base_link()),
+                  action.landmark.pose_stamped.pose);
+        graph.Add("ee", transform_graph::RefFrame("landmark"), action.pose);
+        transform_graph::Transform ee_in_base;
+        graph.ComputeDescription(
+            transform_graph::LocalFrame("ee"),
+            transform_graph::RefFrame(robot_config.base_link()), &ee_in_base);
+        geometry_msgs::Pose pose;
+        ee_in_base.ToPose(&pose);
+        bool success = robot_config.ComputeIk(action.actuator_group, pose,
+                                              &joint_names, &joint_values);
+        if (!success) {
+          ROS_ERROR(
+              "Failed to compute IK for actuator %s on step %ld, action %ld",
+              action.actuator_group.c_str(), step_i, action_i);
+          continue;
+        }
+        for (size_t j = 0; j < joint_names.size(); ++j) {
+          const std::string& name = joint_names[j];
+          double position = joint_values[j];
+          world->joint_state.SetPosition(name, position);
+        }
       }
     }
 
