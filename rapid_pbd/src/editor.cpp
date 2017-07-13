66dd5b2b41b3bdf66682b461ff1522cda28e8b05
Justin Huang <jstn@cs.washington.edu>
2017-07-12 22:57:56 -0700

    Use robot_config to get base link.

--- rapid_pbd/src/editor.cpp
+++ rapid_pbd/src/editor.cpp
@@ -391,8 +391,8 @@ void Editor::GetPose(const std::string& db_id, size_t step_id, size_t action_id,
               landmark_transform);
   } else if (action->landmark.type == msgs::Landmark::SURFACE_BOX) {
     std::string landmark_frame(action->landmark.pose_stamped.header.frame_id);
-    if (landmark_frame != "base_link") {
-      ROS_WARN("Landmark not in base_link frame.");
+    if (landmark_frame != robot_config_.base_link()) {
+      ROS_WARN("Landmark not in base frame.");
     }
     graph.Add("landmark", transform_graph::RefFrame(landmark_frame),
               action->landmark.pose_stamped.pose);
