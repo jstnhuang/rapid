9b57666976518f30305b7339fc5827f7b71ae42b
Justin Huang <jstn@cs.washington.edu>
2018-03-20 15:26:56 -0700

    Updated docs for rapid_ros.

--- rapid_ros/README.md
+++ rapid_ros/README.md
@@ -1,60 +1,15 @@
-# rapid_ros
+rapid_ros {#mainpage}
+=========
 
-rapid_ros contains wrappers around common ROS functionality.
-Note that unlike other rapid packages, the namespace is `rapid_ros`, not `rapid::ros`.
-This is for convenience, otherwise, the `ros` namespace is interpreted as `::rapid::ros` instead of `::ros` inside of other rapid code.
+rapid_ros contains utilities for common ROS features.
 
-Note that tests that use the `MockActionClient` should run as rostests not pure gtests, because it uses ROS logging.
+## ROS Parameters
+When using ROS parameters, it is preferred to set all parameters in a config file and not specify any default values in the code.
+Use functions like rapid::GetStringParamOrThrow, rapid::GetDoubleParamOrThrow, etc. to read parameters from the ROS parameter server.
+These will throw a runtime error if the parameter does not exist.
 
-## Publisher wrapper
-`publisher.h` includes a `PublisherInterface`, which should be used instead of a `ros::Publisher` directly.
-This enables us to write unit tests for classes that use publishers.
+~~~cpp
+#include "rapid_ros/params.h"
 
-Real `Publisher` usage:
-```cpp
-ros::NodeHandle nh;
-PublisherInterface<Marker>* marker_pub = new Publisher<Marker>(nh.advertise<Marker>("markers", 100));
-marker_pub->publish(msg);
-delete marker_pub;
-```
-
-`MockPublisher` usage:
-```cpp
-MockPublisher<Marker> pub;
-PublisherInterface<Marker>* test_pub = &pub;
-test_pub->publish(msg);
-Marker last_message = pub.last_message();
-vector<Marker> history = pub.sent_messages();
-```
-
-## Action client wrapper
-`action_client.h` includes an `ActionClientInterface`, which should be used instead of an `actionlib::SimpleActionClient` directly.
-This is because code using a `SimpleActionClient` cannot be unit-tested without creating a mock action server that runs concurrently with the class under test.
-Our previous experience with this is that these tests will pass on a local development machine, but become flaky inside of Travis.
-Not all functionality of `SimpleActionClient` is in the `ActionClientInterface` yet, but we hope to expand it as needed.
-
-Real `ActionClient` usage:
-```cpp
-ActionClientInterface<MyAction>* client = new ActionClient<MyAction>("my_action");
-client.waitForServer(ros::Duration(10));
-client.sendGoal(goal);
-client.waitForResult(ros::Duration(5));
-MyAction::ResultConstPtr result = client.getResult();
-```
-
-`MockActionClient` usage:
-```cpp
-ActionClientInterface<MyAction>* client = new MockActionClient<MyAction>();
-client.set_server_delay(ros::Duration(11));
-client.waitForServer(ros::Duration(10)); // Will return false;
-client.waitForServer(ros::Duration(12)); // Will return true;
-
-client.set_result_delay(ros::Duration(6));
-client.waitForResult(ros::Duration(7)); // Will return true;
-
-client.set_result(the_result);
-MyAction::ResultConstPtr result = client.getResult(); // result will be equal to the_result
-
-client.sendGoal(goal);
-MyAction::Goal goal = client.last_goal(); // Read the last goal that was sent.
-```
+double foo = rapid::GetDoubleParamOrThrow("foo");
+~~~
--- rapid_ros/include/rapid_ros/params.h
+++ rapid_ros/include/rapid_ros/params.h
@@ -14,7 +14,7 @@ namespace rapid {
 // explicitly list parameters in config files (i.e., in launch files or YAML
 // files) rather than relying on default values set in code.
 
-/// Gets a string param from the parameter server.
+/// Gets a string from the parameter server.
 ///
 /// \param[in] name The name of the parameter in the current node's namespace.
 ///
@@ -22,35 +22,125 @@ namespace rapid {
 /// \throws std::runtime_error if the parameter does not exist.
 std::string GetStringParamOrThrow(const std::string& name);
 
+/// Gets a double from the parameter server.
+///
+/// \param[in] name The name of the parameter in the current node's namespace.
+///
+/// \returns The double parameter
+/// \throws std::runtime_error if the parameter does not exist.
 double GetDoubleParamOrThrow(const std::string& name);
 
+/// Gets a float from the parameter server.
+///
+/// \param[in] name The name of the parameter in the current node's namespace.
+///
+/// \returns The float parameter
+/// \throws std::runtime_error if the parameter does not exist.
 float GetFloatParamOrThrow(const std::string& name);
 
+/// Gets an int from the parameter server.
+///
+/// \param[in] name The name of the parameter in the current node's namespace.
+///
+/// \returns The int parameter
+/// \throws std::runtime_error if the parameter does not exist.
 int GetIntParamOrThrow(const std::string& name);
 
+/// Gets a bool from the parameter server.
+///
+/// \param[in] name The name of the parameter in the current node's namespace.
+///
+/// \returns The bool parameter
+/// \throws std::runtime_error if the parameter does not exist.
 bool GetBoolParamOrThrow(const std::string& name);
 
+/// Gets an XML-RPC value from the parameter server.
+///
+/// \param[in] name The name of the parameter in the current node's namespace.
+///
+/// \returns The XML-RPC value
+/// \throws std::runtime_error if the parameter does not exist.
 XmlRpc::XmlRpcValue GetXmlRpcParamOrThrow(const std::string& name);
 
+/// Gets a vector of strings from the parameter server.
+///
+/// \param[in] name The name of the parameter in the current node's namespace.
+///
+/// \returns The vector of strings
+/// \throws std::runtime_error if the parameter does not exist.
 std::vector<std::string> GetStringVectorParamOrThrow(const std::string& name);
 
+/// Gets a vector of doubles from the parameter server.
+///
+/// \param[in] name The name of the parameter in the current node's namespace.
+///
+/// \returns The vector of doubles
+/// \throws std::runtime_error if the parameter does not exist.
 std::vector<double> GetDoubleVectorParamOrThrow(const std::string& name);
 
+/// Gets a vector of floats from the parameter server.
+///
+/// \param[in] name The name of the parameter in the current node's namespace.
+///
+/// \returns The vector of floats
+/// \throws std::runtime_error if the parameter does not exist.
 std::vector<float> GetFloatVectorParamOrThrow(const std::string& name);
 
+/// Gets a vector of ints from the parameter server.
+///
+/// \param[in] name The name of the parameter in the current node's namespace.
+///
+/// \returns The vector of ints
+/// \throws std::runtime_error if the parameter does not exist.
 std::vector<int> GetIntVectorParamOrThrow(const std::string& name);
 
+/// Gets a vector of bools from the parameter server.
+///
+/// \param[in] name The name of the parameter in the current node's namespace.
+///
+/// \returns The vector of bools
+/// \throws std::runtime_error if the parameter does not exist.
 std::vector<bool> GetBoolVectorParamOrThrow(const std::string& name);
 
+/// Gets a map from strings to string from the parameter server.
+///
+/// \param[in] name The name of the parameter in the current node's namespace.
+///
+/// \returns The map from strings to strings
+/// \throws std::runtime_error if the parameter does not exist.
 std::map<std::string, std::string> GetStringMapParamOrThrow(
     const std::string& name);
 
+/// Gets a map from strings to doubles from the parameter server.
+///
+/// \param[in] name The name of the parameter in the current node's namespace.
+///
+/// \returns The map from strings to doubles
+/// \throws std::runtime_error if the parameter does not exist.
 std::map<std::string, double> GetDoubleMapParamOrThrow(const std::string& name);
 
+/// Gets a map from strings to floats from the parameter server.
+///
+/// \param[in] name The name of the parameter in the current node's namespace.
+///
+/// \returns The map from strings to floats
+/// \throws std::runtime_error if the parameter does not exist.
 std::map<std::string, float> GetFloatMapParamOrThrow(const std::string& name);
 
+/// Gets a map from strings to ints from the parameter server.
+///
+/// \param[in] name The name of the parameter in the current node's namespace.
+///
+/// \returns The map from strings to ints
+/// \throws std::runtime_error if the parameter does not exist.
 std::map<std::string, int> GetIntMapParamOrThrow(const std::string& name);
 
+/// Gets a map from strings to bools from the parameter server.
+///
+/// \param[in] name The name of the parameter in the current node's namespace.
+///
+/// \returns The map from strings to bools
+/// \throws std::runtime_error if the parameter does not exist.
 std::map<std::string, bool> GetBoolMapParamOrThrow(const std::string& name);
 }  // namespace rapid
 
