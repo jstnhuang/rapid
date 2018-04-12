739e10687ee55c9b5f810ddafa122b9e6bd2a050
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-04-12 03:01:50 +0000

    Deploy jstnhuang/rapid to github.com/jstnhuang/rapid.git:gh-pages

--- classrapid_1_1PointCloudCameraInterface.js
+++ classrapid_1_1PointCloudCameraInterface.js
@@ -2,5 +2,5 @@ var classrapid_1_1PointCloudCameraInterface =
 [
     [ "~PointCloudCameraInterface", "classrapid_1_1PointCloudCameraInterface.html#a105ad24106c21cf346e3fde3db64b647", null ],
     [ "camera_pose", "classrapid_1_1PointCloudCameraInterface.html#a10ab465fe6fe1ba76741583d1e168551", null ],
-    [ "cloud", "classrapid_1_1PointCloudCameraInterface.html#a8ac9aff70aedc1345c01ae9b10614eae", null ]
+    [ "cloud", "classrapid_1_1PointCloudCameraInterface.html#a83a67aac13a0955c7bc939912d6ee4fb", null ]
 ];
