4b85d585cecce9d6397fb26c8820e9fd63b665ae
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-04-12 01:43:02 +0000

    Deploy jstnhuang/rapid to github.com/jstnhuang/rapid.git:gh-pages

--- classrapid_1_1PointCloudCamera.js
+++ classrapid_1_1PointCloudCamera.js
@@ -2,5 +2,5 @@ var classrapid_1_1PointCloudCamera =
 [
     [ "PointCloudCamera", "classrapid_1_1PointCloudCamera.html#a922f533e7e81acd8fec3814079f417a9", null ],
     [ "camera_pose", "classrapid_1_1PointCloudCamera.html#a4902874639f4cbd1ea2fdd38728cbc4a", null ],
-    [ "cloud", "classrapid_1_1PointCloudCamera.html#ab9d777ca33a19a45c6df571e584be612", null ]
+    [ "cloud", "classrapid_1_1PointCloudCamera.html#a369894537c37ad0330127a86a9180be4", null ]
 ];
--- classrapid_1_1RecordedPointCloudCamera.js
+++ classrapid_1_1RecordedPointCloudCamera.js
@@ -2,6 +2,6 @@ var classrapid_1_1RecordedPointCloudCamera =
 [
     [ "RecordedPointCloudCamera", "classrapid_1_1RecordedPointCloudCamera.html#a4a06734c33bd6e9ea3444bf640ea9d40", null ],
     [ "camera_pose", "classrapid_1_1RecordedPointCloudCamera.html#a80bdf0e09a3cd87177d27ad3731e343d", null ],
-    [ "cloud", "classrapid_1_1RecordedPointCloudCamera.html#ae7dc31e1ff6ad178b2e38584f54a3c04", null ],
+    [ "cloud", "classrapid_1_1RecordedPointCloudCamera.html#ac1570d700e57fae34ce3ba8e8887878a", null ],
     [ "LoadBag", "classrapid_1_1RecordedPointCloudCamera.html#a5a8fe4e2c9fb9e7d77f1188ab59be4b7", null ]
 ];
