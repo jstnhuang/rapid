13a49397f6c67956a7fc651fbfa58b3a333750e6
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-05-01 20:49:52 +0000

    Deploy jstnhuang/rapid to github.com/jstnhuang/rapid.git:gh-pages

--- publish_8h.js
+++ publish_8h.js
@@ -2,6 +2,7 @@ var publish_8h =
 [
     [ "PublishBlankCloud", "publish_8h.html#a09031f72d99862ae2e106e8101e111df", null ],
     [ "PublishCloud", "publish_8h.html#a333c32dbe655e3c6eec67ef55574ae88", null ],
+    [ "PublishCloud", "publish_8h.html#a6c187c88c2c6c81d376083f7014d34ec", null ],
     [ "PublishCloud", "publish_8h.html#a0009d5dc4fff091e68a784f3ed1c4734", null ],
     [ "PublishCloud", "publish_8h.html#aaa52c46f4bb82765869024a07ef03a16", null ],
     [ "PublishImage", "publish_8h.html#a00bea7ca2f322f7caf752c0c1b7ce6e7", null ]
